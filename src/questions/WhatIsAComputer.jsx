88c2d55f135da1551ee9ea3f5d6e37d1b27bf3e4
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-22 19:34:58 -0400

    feat: add questios 1

---
+++ src/questions/WhatIsAComputer.jsx
@@ -0,0 +1,18 @@
+
+
+const WhatsIsAComputer = () => {
+  return (
+    <div>
+      <h1>
+        ¿Que es un computador?
+      </h1>
+      <p>
+        Un computador es una máquina electrónica que está diseñada para realizar tareas específicas.
+        En muchos países se le conoce como computadora u ordenador, pero todas estas palabras se
+        refieren a lo mismo.
+      </p>
+    </div>
+  )
+};
+
+export default WhatsIsAComputer;
