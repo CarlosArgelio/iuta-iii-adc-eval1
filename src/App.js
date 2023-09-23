1dd3fb0506453c69fb8f15674561843af8678bbb
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-23 05:46:03 -0400

    fix: rm components in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-import WhatsIsAComputer from './pages/questions/WhatIsAComputer';
-import TypesComputers from './pages/questions/TypesComputers';
-import CharacteristicsVariousComputers from './pages/questions/CharacteristicsVariousComputers';
-import CharacteristicsVariosComputersDependingDataHandling from './pages/questions/CharacteristicsVariosComputersDependingDataHandling';
 import Author from './pages/author/authors';
 import NavBar from './pages/navbar/navbar';
 
@@ -11,12 +7,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-
-      <WhatsIsAComputer />
-      <TypesComputers />
-      <CharacteristicsVariousComputers />
-      <CharacteristicsVariosComputersDependingDataHandling />
-
       <Author />
     </div>
   );
