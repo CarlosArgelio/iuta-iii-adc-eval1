7958c2e0c802cebcc24ceceb36233163ee60b5e4
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-23 05:56:25 -0400

    refactor: question one to boostrap Card

--- src/pages/questions/WhatIsAComputer.jsx
+++ src/pages/questions/WhatIsAComputer.jsx
@@ -1,18 +1,24 @@
+import Card from 'react-bootstrap/Card';
+import computerIMage from '../../assets/questionsOne/resource_computer_1.jpg'
 
 
-const WhatsIsAComputer = () => {
+function WhatsIsAComputer() {
   return (
-    <div>
-      <h1>
-        ¿Que es un computador?
-      </h1>
-      <p>
-        Un computador es una máquina electrónica que está diseñada para realizar tareas específicas.
-        En muchos países se le conoce como computadora u ordenador, pero todas estas palabras se
-        refieren a lo mismo.
-      </p>
-    </div>
-  )
-};
+    <Card style={{
+      width: '18rem',
+      margin: 'auto',
+    }}>
+      <Card.Img variant="top" src={computerIMage} />
+      <Card.Body>
+        <Card.Title> ¿Que es un computador? </Card.Title>
+        <Card.Text>
+          Un computador es una máquina electrónica que está diseñada para realizar tareas específicas.
+          En muchos países se le conoce como computadora u ordenador, pero todas estas palabras se
+          refieren a lo mismo.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export default WhatsIsAComputer;
