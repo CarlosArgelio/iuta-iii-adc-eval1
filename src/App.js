e1247ad207d1fd2eb8ac32dfcf0b2a53cfd0e71e
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-23 05:04:02 -0400

    fix: passed all components to new folders 'pages'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import WhatsIsAComputer from './questions/WhatIsAComputer';
-import TypesComputers from './questions/TypesComputers';
-import CharacteristicsVariousComputers from './questions/CharacteristicsVariousComputers';
-import CharacteristicsVariosComputersDependingDataHandling from './questions/CharacteristicsVariosComputersDependingDataHandling';
-import Author from './author/authors';
+import WhatsIsAComputer from './pages/questions/WhatIsAComputer';
+import TypesComputers from './pages/questions/TypesComputers';
+import CharacteristicsVariousComputers from './pages/questions/CharacteristicsVariousComputers';
+import CharacteristicsVariosComputersDependingDataHandling from './pages/questions/CharacteristicsVariosComputersDependingDataHandling';
+import Author from './pages/author/authors';
 
 
 function App() {
