2851d3c98ffe14a2916241e2268466e44f4cd2a9
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-23 05:44:21 -0400

    fix: add routes to navbat

--- src/pages/navbar/navbar.jsx
+++ src/pages/navbar/navbar.jsx
@@ -9,10 +9,10 @@ function NavBar() {
         <Container>
           <Navbar.Brand href="#home">Info</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Pregunta 1</Nav.Link>
-            <Nav.Link href="#features">Pregunta 2</Nav.Link>
-            <Nav.Link href="#pricing">Pregunta 3</Nav.Link>
-            <Nav.Link href="#pricing">Pregunta 4</Nav.Link>
+            <Nav.Link href="/questions-one">Pregunta 1</Nav.Link>
+            <Nav.Link href="/questions-two">Pregunta 2</Nav.Link>
+            <Nav.Link href="/questions-three">Pregunta 3</Nav.Link>
+            <Nav.Link href="/questions-four">Pregunta 4</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
