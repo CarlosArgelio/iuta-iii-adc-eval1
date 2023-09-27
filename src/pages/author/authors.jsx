3529cec50f70eb1001c90a038a6c70b9cc8070d8
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-26 21:05:24 -0400

    fix: auhor

--- src/pages/author/authors.jsx
+++ src/pages/author/authors.jsx
@@ -1,7 +1,21 @@
+import logoAuthor from '../../assets/logoAuthor.jpeg'
 
 const Author = () => {
   return (
     <div className="author">
+      <p>
+        IUTA
+        <br />
+        Tercer Cuatrimeste
+      </p>
+      <div>
+        <img style={{
+          width: '150px',
+          height: '200px',
+          borderRadius: '28px',
+          objectFit: 'cover'
+        }} src={logoAuthor} alt="Carlos Argelio" />
+      </div>
       <p>
         <a href="https://github.com/CarlosArgelio">Carlos A. Palacios R.</a>
       </p>
